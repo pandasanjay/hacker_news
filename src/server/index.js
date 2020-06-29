9c2c2177be43cc85ba1f7978fe7e3fe338771d47
Sanjay <panda.sanjay18@gmail.com>
2020-06-29 19:58:07 +0100

    STORY-4 PWA and service worker

--- src/server/index.js
+++ src/server/index.js
@@ -19,7 +19,7 @@ app.use(
     graphqlHTTP({
         schema: schema,
         rootValue: root,
-        graphiql: true,
+        graphiql: process.env.NODE_ENV !== 'production',
     })
 )
 app.use(express.static('dist/public'))
