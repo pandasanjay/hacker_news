0dd3a4817d06494152c7bcd54e831cd1f40444bb
Sanjay <panda.sanjay18@gmail.com>
2020-06-30 11:49:16 +0100

    STORY-6 Upvote miner issue fix

--- src/store/root-saga.js
+++ src/store/root-saga.js
@@ -1,4 +1,4 @@
-import { all, takeLatest } from 'redux-saga/effects'
+import { all, takeLatest, takeEvery } from 'redux-saga/effects'
 import types from './combine-types'
 import { getStoriesFromApi } from './sagas/getStorySaga'
 import { updateVote } from './sagas/upVoteSaga'
@@ -11,7 +11,7 @@ function* watchForUpVoteRequest() {
     yield takeLatest(types.SET_UP_VOTE_PER_STORY, updateVote)
 }
 function* watchForHideStoryRequest() {
-    yield takeLatest(types.HIDE_STORY_FROM_LIST, hideStory)
+    yield takeEvery(types.HIDE_STORY_FROM_LIST, hideStory)
 }
 export default function* rootSaga() {
     yield all([
